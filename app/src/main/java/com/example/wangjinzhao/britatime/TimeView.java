8fde7f1303ed179315d62511ec97b6ea71b606a0
wjz <wjz@163.com>
2016-07-29 14:10:14 +0800

    optimize

--- app/src/main/java/com/example/wangjinzhao/britatime/TimeView.java
+++ app/src/main/java/com/example/wangjinzhao/britatime/TimeView.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
@@ -17,34 +18,36 @@ public class TimeView extends View {
 
     Context context;
     long leftDay;
-    public TimeView(Context context, AttributeSet attributeSet)
-    {
-        super(context,attributeSet);
-        this.context=context;
+    Paint paint = new Paint();
+    Paint textPaint = new Paint();
+    Rect rect = new Rect();
+
+    public TimeView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        this.context = context;
+        paint.setColor(ContextCompat.getColor(context, R.color.colorPrimary));
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        textPaint.setColor(Color.WHITE);
+        textPaint.setTextSize(100);
+        textPaint.setAntiAlias(true);
     }
 
     @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
-        paint.setColor(ContextCompat.getColor(context, R.color.colorPrimary));
-        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        long Y= canvas.getHeight()*leftDay/30;
-        canvas.drawRect(canvas.getClipBounds().left,canvas.getClipBounds().bottom-Y, canvas.getClipBounds().right, canvas.getClipBounds().bottom, paint);
-        Paint textPaint=new Paint();
-        textPaint.setColor(Color.WHITE);
-        textPaint.setTextSize(100);
-        textPaint.setAntiAlias(true);
-        long halfY=Y/2;
-        int halfX=canvas.getWidth()/2;
-        int left=canvas.getClipBounds().left+halfX-50;
-        long top=canvas.getClipBounds().bottom-Y+halfY+50;
-        canvas.drawText(String.valueOf(leftDay),left,top,textPaint);
+        long Y = canvas.getHeight() * leftDay / 30;
+        canvas.getClipBounds(rect);
+        canvas.drawRect(rect.left, rect.bottom - Y, rect.right, rect.bottom, paint);
+
+        long halfY = Y / 2;
+        int halfX = canvas.getWidth() / 2;
+        int left = rect.left + halfX - 50;
+        long top = rect.bottom - Y + halfY + 50;
+        canvas.drawText(String.valueOf(leftDay), left, top, textPaint);
         super.onDraw(canvas);
     }
 
-    public void setLeftDay(long leftDay)
-    {
-        this.leftDay=leftDay;
+    public void setLeftDay(long leftDay) {
+        this.leftDay = leftDay;
     }
 }
